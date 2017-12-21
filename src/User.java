6b544db9ed2594180ab9b352350b0cc6ba4331fc
sandaru <sandaru@RuZZa-PC>
2017-12-21 14:51:42 +0530

    login screen code with changes to new screens.

---
+++ src/User.java
@@ -0,0 +1,56 @@
+
+import javax.swing.JFrame;
+
+
+
+
+
+
+
+
+
+
+public class User {
+    public int uId;
+    public char un;
+    private char pwd;
+    
+    public void login()
+    {
+        
+    }
+    
+    
+    public void newUser()
+    {
+      new newUser().setVisible(true);
+     
+    }
+    
+    public void forgotPassword()
+    {
+        
+    }
+    
+    public void register()
+    {
+        
+    }
+    
+    public void clear()
+    {
+        
+    }
+    
+    public void registerAnotherUser()
+    {
+        
+    }
+}
+
+        
+        
+        
+    
+
+
